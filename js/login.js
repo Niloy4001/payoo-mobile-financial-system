a1c9fc7ee183a9e68fca5ce1b33de595f5cd4abb
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-24 16:05:11 +0600

    restarted the project

--- js/login.js
+++
@@ -1,9 +0,0 @@
-loginBtn.addEventListener("click", function () {
-  if (loginPin.value === "1234") {
-    loginForm.classList.add("hidden");
-  }
-});
-
-logOutBtn.addEventListener("click", function () {
-  loginForm.classList.remove("hidden");
-});
